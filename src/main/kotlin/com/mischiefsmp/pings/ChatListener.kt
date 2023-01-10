be9640806508db398f3d43e46401bf3cf1d697b0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2023-01-10 12:16:06 +0100

    Add everyone, someone

--- src/main/kotlin/com/mischiefsmp/pings/ChatListener.kt
+++ src/main/kotlin/com/mischiefsmp/pings/ChatListener.kt
@@ -8,14 +8,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent
 class ChatListener: Listener {
     @EventHandler
     fun onChatAsync(event: AsyncPlayerChatEvent) {
-        val pl = MischiefPings.pl
-        val names = TagUtils.getUsernames(event.message)
-        val foundUsers = mutableListOf<Player>()
-        names.forEach {
-            val p = pl.server.getPlayerExact(it.removePrefix("@"))
-            if(p != null) foundUsers.add(p)
-        }
-        foundUsers.forEach {
+        TagUtils.getUsers(event.message).forEach {
             it.sendTitle("Ping!", "You've been pinged by §1${event.player.name}.", 1, 40, 1)
         }
         event.message = TagUtils.colorText(event.message)
--- src/main/kotlin/com/mischiefsmp/pings/TagUtils.kt
+++ src/main/kotlin/com/mischiefsmp/pings/TagUtils.kt
@@ -1,20 +1,33 @@
 package com.mischiefsmp.pings
 
+import org.bukkit.entity.Player
+
 object TagUtils {
     private const val tagRegex = "(?<!\\S)(@[^@]*?)(?=[ .?,!]|\$)"
+    private val pl = MischiefPings.pl
 
     fun colorText(text: String): String {
-        val pl = MischiefPings.pl
         return Regex(tagRegex).replace(text){
-            val p = pl.server.getPlayerExact(it.value.removePrefix("@"))
-            if(p == null) it.value
-            else "§2${it.value}§r"
+            val rawValue = it.value.removePrefix("@")
+            val p = pl.server.getPlayerExact(rawValue)
+            if(p == null && rawValue != "everyone" && rawValue != "someone") it.value
+            else "§e${it.value}§r"
         }
     }
 
-    fun getUsernames(text: String): List<String> {
-        val list = mutableListOf<String>()
-        Regex(tagRegex).findAll(text).forEach { list.add(it.value) }
+    fun getUsers(text: String): List<Player> {
+        val list = mutableListOf<Player>()
+        Regex(tagRegex).findAll(text).forEach {
+            val rawValue = it.value.lowercase().removePrefix("@")
+            when(it.value.lowercase().removePrefix("@")) {
+                "everyone" -> return pl.server.onlinePlayers.toList()
+                "someone" -> return listOf(pl.server.onlinePlayers.random())
+                else -> pl.server.getPlayer(rawValue)?.let { player ->
+                    list.add(player)
+                }
+            }
+        }
+
         return list
     }
 }
